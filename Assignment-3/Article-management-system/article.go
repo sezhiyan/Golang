b0b4d44734ba080abb05fd3609590509a0b3cca5
Dhanasezhiyan S <68005980+sezhiyan@users.noreply.github.com>
2021-03-05 16:34:42 +0530

    Add files via upload

---
+++ Assignment-3/Article-management-system/article.go
@@ -0,0 +1,87 @@
+// main.go go rest API
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Article - Our struct for all articles
+type Article struct {
+	ID          string `json:"Id"`
+	Firstname   string `json:"Firstname"`
+	Lastname    string `json:"Lastname"`
+	Designation string `json:"Designation"`
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["ID"]
+
+	for _, article := range Articles {
+		if article.ID == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a creating new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	// update our global Articles array to include
+	// our new Article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["ID"]
+
+	for index, article := range Articles {
+		if article.ID == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{ID}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{ID}", returnSingleArticle)
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+
+func main() {
+	Articles = []Article{
+		Article{ID: "1", Firstname: "arun", Lastname: "kumar", Designation: "programmer"},
+		Article{ID: "2", Firstname: "praveen", Lastname: "raj", Designation: "Driver"},
+	}
+	handleRequests()
+}
